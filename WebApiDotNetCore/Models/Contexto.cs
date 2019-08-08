c1c09a0ffb230aef4e753bdada8908df194c8c0d
Cássio Matos <cassiolrm30@gmail.com>
2019-08-07 22:45:42 -0300

    second commit

---
+++ WebApiDotNetCore/Models/Contexto.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiDotNetCore.Models;
+
+public partial class Contexto : DbContext
+{
+    public Contexto()
+    {
+    }
+
+    public Contexto(DbContextOptions<Contexto> options) : base(options)
+    {
+    }
+
+    public virtual DbSet<Livro> Livros { get; set; }
+
+    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //{
+    //    if (!optionsBuilder.IsConfigured)
+    //    {
+    //        optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=WebApiDotNetCore;Trusted_Connection=True;");
+    //    }
+    //}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Livro>(entity =>
+        {
+            entity.Property(e => e.Titulo).IsRequired();
+        });
+    }
+}
---
+++ WebApiDotNetCore/Startup.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebApiDotNetCore.Models;
+
+namespace WebApiDotNetCore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddEntityFrameworkSqlServer();
+            services.AddDbContext<Contexto>(options => options.UseSqlServer(Configuration.GetConnectionString("Conexao")));
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+
+            app.UseMvcWithDefaultRoute();
+            app.UseCors(x => x
+                    .AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+            app.UseMvc();
+        }
+    }
+}
